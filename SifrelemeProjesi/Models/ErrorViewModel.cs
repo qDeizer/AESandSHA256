1c8555537ac78f0cabe6b1c9e9e462a037bead6e
qDeizer <88134658+qDeizer@users.noreply.github.com>
2025-06-10 10:39:20 +0300

    Delete SifrelemeProjesi directory

--- SifrelemeProjesi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SifrelemeProjesi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
